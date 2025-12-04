549ccae46b71ad8f5583b5ec237f3d86c0e3c2bf
AENCRUZ <angelyncruzat123@gmail.com>
2025-12-04 13:52:13 +0800

    Update MoodCycle.java

--- FlowCtrl/MoodCycle.java
+++ FlowCtrl/MoodCycle.java
@@ -2,10 +2,14 @@ public class MoodCycle extends Cycle {
     public MoodCycle(String startDate, String endDate, String symptoms, String mood, int moodValue) {
         super(startDate, endDate, symptoms, mood, moodValue);
     }
+
     @Override
     public void displayCycleInfo(int count) {
-        String enhancedMood = getMood();
-        System.out.printf("%-3d | %-12s | %-12s | %-8d | %-25s | %-15s%n",
+        // Customized for MoodCycle: Enhance the mood display with extra formatting to emphasize mood-tracking
+        String enhancedMood = "Mood: " + getMood() + " (Value: " + getMoodValue() + ")";  
+        System.out.printf("%-3d | %-12s | %-12s | %-8d | %-25s | %-25s%n",  
                 count, getStartDate(), getEndDate(), getCycleLength(), getSymptoms(), enhancedMood);
     }
 }
+
+
