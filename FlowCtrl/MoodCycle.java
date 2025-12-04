2540625295fa239276dfa7bd45f2814d192fc3ab
AENCRUZ <angelyncruzat123@gmail.com>
2025-12-04 13:12:32 +0800

    Update MoodCycle.java

--- FlowCtrl/MoodCycle.java
+++ FlowCtrl/MoodCycle.java
@@ -1,13 +1,11 @@
 public class MoodCycle extends Cycle {
-
     public MoodCycle(String startDate, String endDate, String symptoms, String mood, int moodValue) {
         super(startDate, endDate, symptoms, mood, moodValue);
     }
-
     @Override
     public void displayCycleInfo(int count) {
-        System.out.println("=== MoodCycle Details ===");
-        super.displayCycleInfo(count);
-        System.out.println("------------------------");
+        String enhancedMood = getMood();
+        System.out.printf("%-3d | %-12s | %-12s | %-8d | %-25s | %-15s%n",
+                count, getStartDate(), getEndDate(), getCycleLength(), getSymptoms(), enhancedMood);
     }
 }
